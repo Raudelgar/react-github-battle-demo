3b1b545d9d15e521db92a6ed99477e251eb1fcb9
raudelgar <raudel24@gmail.com>
2020-03-28 18:32:40 -0400

    refactor react router implementation

--- app/components/navbar/NavBar.js
+++ app/components/navbar/NavBar.js
@@ -1,65 +1,47 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { FaToggleOn, FaToggleOff } from 'react-icons/fa';
 
 import { ThemeConsumer } from '../context/ThemeContext.js';
 
-export default class NavBar extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			tab: 'Popular'
-		};
-
-		this.updateTab = this.updateTab.bind(this);
-	}
-
-	updateTab(tab) {
-		this.setState({ tab });
-	}
-
-	render() {
-		const { tab } = this.state;
-		const linkStyle = {
-			color: 'inherit',
-			textDecoration: 'none'
-		};
-		return (
-			<ThemeConsumer>
-				{({ theme, toggleTheme }) => (
-					<React.Fragment>
-						<ul className='nav'>
-							<Link to='/' style={linkStyle}>
-								<button
-									className={`nav btn-clear ${
-										tab === 'Popular' ? 'selectedBtn' : ''
-									}`}
-									onClick={() => this.updateTab('Popular')}
-								>
-									Popular
-								</button>
-							</Link>
-							<Link to='/battle' style={linkStyle}>
-								<button
-									className={`nav btn-clear ${
-										tab === 'Battle' ? 'selectedBtn' : ''
-									}`}
-									onClick={() => this.updateTab('Battle')}
-								>
-									Battle
-								</button>
-							</Link>
-						</ul>
-						<button className='nav btn-clear theme' onClick={toggleTheme}>
-							{theme === 'light' ? (
-								<FaToggleOff color='#414344' size={35} />
-							) : (
-								<FaToggleOn color='#dedee0' size={35} />
-							)}
-						</button>
-					</React.Fragment>
-				)}
-			</ThemeConsumer>
-		);
-	}
+export default function NavBar() {
+	const activeLinkStyle = {
+		color: '#9c1010'
+	};
+	return (
+		<ThemeConsumer>
+			{({ theme, toggleTheme }) => (
+				<nav className='row space-between'>
+					<ul className='row nav'>
+						<li>
+							<NavLink
+								to='/'
+								exact
+								className='navb-link'
+								activeStyle={activeLinkStyle}
+							>
+								Popular
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								to='/battle'
+								className='navb-link'
+								activeStyle={activeLinkStyle}
+							>
+								Battle
+							</NavLink>
+						</li>
+					</ul>
+					<button className='btn-clear' onClick={toggleTheme}>
+						{theme === 'light' ? (
+							<FaToggleOff color='#414344' size={30} />
+						) : (
+							<FaToggleOn color='#dedee0' size={30} />
+						)}
+					</button>
+				</nav>
+			)}
+		</ThemeConsumer>
+	);
 }
