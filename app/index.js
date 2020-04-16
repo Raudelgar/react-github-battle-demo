0084756267c9556244b15bd19684e0434b8b5f85
raudelgar <raudel24@gmail.com>
2020-04-16 16:48:49 -0400

    testing space vs tabs

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-import React, { Component, lazy, Suspense, useState } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
