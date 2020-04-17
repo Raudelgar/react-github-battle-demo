9e232059e2ebdaa0e4cd7aaa0acd55725d692204
raudelgar <raudel24@gmail.com>
2020-04-17 12:13:45 -0400

    refactor hover to custom hook

--- app/components/tooltips/Tooltip.js
+++ app/components/tooltips/Tooltip.js
@@ -1,34 +1,20 @@
-import React, { useContext, useRef, useEffect } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 
 // import withHover from '../../utils/withHover.js';
-import Hover from '../../utils/Hover.js';
+// import Hover from '../../utils/Hover.js';
+import useHover from '../../utils/useHover.js';
 import ThemeContext from '../context/ThemeContext';
 
 export default function Tooltip({ content = 'Tooltip Content', children }) {
 	const { theme } = useContext(ThemeContext);
-	const toolRef = useRef();
+	const [hovering, attr] = useHover();
 
-	useEffect(() => {
-		// console.log('toolRef', toolRef.current);
-	});
 	return (
-		<Hover>
-			{(hover, styles) => (
-				<div className='tool-container'>
-					{hover && (
-						<div
-							className={`${theme}-tooltip`}
-							// style={styles ? styles : ''}
-							// ref={toolRef}
-						>
-							{content}
-						</div>
-					)}
-					{children}
-				</div>
-			)}
-		</Hover>
+		<div className='tool-container' {...attr}>
+			{hovering && <div className={`${theme}-tooltip`}>{content}</div>}
+			{children}
+		</div>
 	);
 }
 
